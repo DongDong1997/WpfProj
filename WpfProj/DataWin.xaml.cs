0db53439a62f059e0b9882cb92f8424fa5bfc328
Peidong Guo <1400951712@qq.com>
2024-08-07 00:04:54 +0800

    fix:整理总页面，补充数据、模板，绑定窗口test

---
+++ WpfProj/DataWin.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfProj
+{
+    /// <summary>
+    /// DataWin.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataWin : Window
+    {
+        public DataWin()
+        {
+            InitializeComponent();
+
+            List<Color> ints = new List<Color>();
+
+            ints.Add(new Color() { Code = "#FFDAB9", Name = "PeachPuff" });
+            ints.Add(new Color() { Code = "#E6E6FA", Name = "lavender" });
+            ints.Add(new Color() { Code = "#7B68EE", Name = "MediumSlateBlue" });
+            ints.Add(new Color() { Code = "#CD5C5C", Name = "IndianRed" });
+
+            DataList.ItemsSource = ints;
+
+            dataGrid.ItemsSource = ints;
+        }
+    }
+
+    public class Color
+    {
+        public string Code {  get; set; }
+        public string Name { get; set; }
+    }
+}
--- WpfProj/MainWindow.xaml.cs
+++ WpfProj/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace WpfProj
         {
             InitializeComponent();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ArrangeWin arrangeWin = new ArrangeWin();
+            arrangeWin.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            StyleWin styleWin = new StyleWin();
+            styleWin.Show();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ControlWin controlWin = new ControlWin();
+            controlWin.Show();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            DataWin dataWin = new DataWin();
+            dataWin.Show();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            BindingWin bindingWin = new BindingWin();
+            bindingWin.Show();
+        }
     }
 }
